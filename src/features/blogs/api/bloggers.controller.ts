91af92d187cff64f23700df4b2eb48c1fd560445
restitution <sniffjoke@gmail.com>
2025-08-08 01:14:25 +0200

    add endpoint for get-users-with-ban

--- src/features/blogs/api/bloggers.controller.ts
+++ src/features/blogs/api/bloggers.controller.ts
@@ -7,29 +7,27 @@ import {
   Param,
   Post,
   Put,
-  Query, Req,
+  Query,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { BlogCreateModel } from './models/input/create-blog.input.model';
 import { PostCreateModelWithParams } from '../../posts/api/models/input/create-post.input.model';
 import { PostsService } from '../../posts/application/posts.service';
-import { Request} from 'express';
+import { Request } from 'express';
 import { CommandBus } from '@nestjs/cqrs';
 import { CreateBlogCommand } from '../application/useCases/create-blog.use-case';
 import { UpdateBlogCommand } from '../application/useCases/update-blog.use-case';
 import { DeleteBlogCommand } from '../application/useCases/delete-blog.use-case';
-import {
-  UpdatePostWithBlogInParamsCommand,
-} from '../../posts/application/useCases/update-post-from-blogs-in-params.use-case';
+import { UpdatePostWithBlogInParamsCommand } from '../../posts/application/useCases/update-post-from-blogs-in-params.use-case';
 import { DeletePostWithBlogInParamsCommand } from '../../posts/application/useCases/delete-post-from-blogs-in-params.use-case';
 import { CreatePostCommand } from '../../posts/application/useCases/create-post.use-case';
 import { BlogsQueryRepositoryTO } from '../infrastructure/blogs.query-repository.to';
 import { PostsQueryRepositoryTO } from '../../posts/infrastructure/posts.query-repository.to';
-import {JwtAuthGuard} from "../../../core/guards/jwt-auth.guard";
-import {UsersService} from "../../users/application/users.service";
+import { JwtAuthGuard } from '../../../core/guards/jwt-auth.guard';
+import { UsersService } from '../../users/application/users.service';
 import { BlogsService } from '../application/blogs.service';
 import { BanInfoForUserDto } from './models/input/ban-user-for-blog.dto';
-
 
 @Controller('blogger')
 export class BloggersController {
@@ -39,16 +37,17 @@ export class BloggersController {
     private readonly postsService: PostsService,
     private readonly postsQueryRepository: PostsQueryRepositoryTO,
     private readonly usersService: UsersService,
-    private readonly blogsService: BlogsService
-  ) {
-  }
+    private readonly blogsService: BlogsService,
+  ) {}
 
-// TODO: метод execute pattern (service)
+  // TODO: метод execute pattern (service)
 
   @Post('blogs')
   @UseGuards(JwtAuthGuard)
   async createBlog(@Body() dto: BlogCreateModel, @Req() req: Request) {
-    const blogId = await this.commandBus.execute(new CreateBlogCommand(dto, req.headers.authorization as string));
+    const blogId = await this.commandBus.execute(
+      new CreateBlogCommand(dto, req.headers.authorization as string),
+    );
     const newBlog = await this.blogsQueryRepository.blogOutput(blogId);
     return newBlog;
   }
@@ -56,16 +55,28 @@ export class BloggersController {
   @Get('blogs') //-1
   @UseGuards(JwtAuthGuard)
   async getAll(@Query() query: any, @Req() req: Request) {
-    const user = await this.usersService.getUserByAuthToken(req.headers.authorization as string);
-    const blogsWithQuery = await this.blogsQueryRepository.getAllBlogsWithQuery(query, false, user.id);
+    const user = await this.usersService.getUserByAuthToken(
+      req.headers.authorization as string,
+    );
+    const blogsWithQuery = await this.blogsQueryRepository.getAllBlogsWithQuery(
+      query,
+      false,
+      user.id,
+    );
     return blogsWithQuery;
   }
 
   @Put('blogs/:id')
   @HttpCode(204)
   @UseGuards(JwtAuthGuard)
-  async updateBlogById(@Param('id') id: string, @Body() dto: BlogCreateModel, @Req() req: Request) {
-    const updateBlog = await this.commandBus.execute(new UpdateBlogCommand(id, dto, req.headers.authorization as string));
+  async updateBlogById(
+    @Param('id') id: string,
+    @Body() dto: BlogCreateModel,
+    @Req() req: Request,
+  ) {
+    const updateBlog = await this.commandBus.execute(
+      new UpdateBlogCommand(id, dto, req.headers.authorization as string),
+    );
     return updateBlog;
   }
 
@@ -73,7 +84,9 @@ export class BloggersController {
   @HttpCode(204)
   @UseGuards(JwtAuthGuard)
   async deleteBlog(@Param('id') id: string, @Req() req: Request) {
-    const deleteBlog = await this.commandBus.execute(new DeleteBlogCommand(id, req.headers.authorization as string));
+    const deleteBlog = await this.commandBus.execute(
+      new DeleteBlogCommand(id, req.headers.authorization as string),
+    );
     return deleteBlog;
   }
 
@@ -81,9 +94,19 @@ export class BloggersController {
 
   @Get('blogs/:id/posts')
   @UseGuards(JwtAuthGuard)
-  async getAllPostsByBlogId(@Param('id') id: string, @Query() query: any, @Req() req: Request) {
-    const posts = await this.postsQueryRepository.getAllPostsWithQuery(query, id);
-    const newData = await this.postsService.generatePostsWithLikesDetails(posts.items, req.headers.authorization as string);
+  async getAllPostsByBlogId(
+    @Param('id') id: string,
+    @Query() query: any,
+    @Req() req: Request,
+  ) {
+    const posts = await this.postsQueryRepository.getAllPostsWithQuery(
+      query,
+      id,
+    );
+    const newData = await this.postsService.generatePostsWithLikesDetails(
+      posts.items,
+      req.headers.authorization as string,
+    );
     return {
       ...posts,
       items: newData,
@@ -92,18 +115,45 @@ export class BloggersController {
 
   @Post('blogs/:id/posts')
   @UseGuards(JwtAuthGuard)
-  async createPostWithParams(@Body() dto: PostCreateModelWithParams, @Param('id') blogId: string, @Req() req: Request) {
-    const postId = await this.commandBus.execute(new CreatePostCommand({ ...dto, blogId }, req.headers.authorization as string));
+  async createPostWithParams(
+    @Body() dto: PostCreateModelWithParams,
+    @Param('id') blogId: string,
+    @Req() req: Request,
+  ) {
+    const postId = await this.commandBus.execute(
+      new CreatePostCommand(
+        {
+          ...dto,
+          blogId,
+        },
+        req.headers.authorization as string,
+      ),
+    );
     const newPost = await this.postsQueryRepository.postOutput(postId);
-    const postWithDetails = await this.postsService.generateOnePostWithLikesDetails(newPost, req.headers.authorization as string);
+    const postWithDetails =
+      await this.postsService.generateOnePostWithLikesDetails(
+        newPost,
+        req.headers.authorization as string,
+      );
     return postWithDetails;
   }
 
   @Put('blogs/:blogId/posts/:postId')
   @HttpCode(204)
   @UseGuards(JwtAuthGuard)
-  async updatePost(@Body() dto: PostCreateModelWithParams, @Param() idParams: any, @Req() req: Request) {
-    const updatePost = await this.commandBus.execute(new UpdatePostWithBlogInParamsCommand(idParams.postId, idParams.blogId, dto, req.headers.authorization as string));
+  async updatePost(
+    @Body() dto: PostCreateModelWithParams,
+    @Param() idParams: any,
+    @Req() req: Request,
+  ) {
+    const updatePost = await this.commandBus.execute(
+      new UpdatePostWithBlogInParamsCommand(
+        idParams.postId,
+        idParams.blogId,
+        dto,
+        req.headers.authorization as string,
+      ),
+    );
     return updatePost;
   }
 
@@ -111,7 +161,13 @@ export class BloggersController {
   @HttpCode(204)
   @UseGuards(JwtAuthGuard)
   async deletePost(@Param() idParams: any, @Req() req: Request) {
-    const deletePost = await this.commandBus.execute(new DeletePostWithBlogInParamsCommand(idParams.postId, idParams.blogId, req.headers.authorization as string));
+    const deletePost = await this.commandBus.execute(
+      new DeletePostWithBlogInParamsCommand(
+        idParams.postId,
+        idParams.blogId,
+        req.headers.authorization as string,
+      ),
+    );
     return deletePost;
   }
 
@@ -120,12 +176,32 @@ export class BloggersController {
   @Put('users/:id/ban')
   @HttpCode(204)
   @UseGuards(JwtAuthGuard)
-  async banUserForBlog(@Param('id') id: string, @Body() dto: BanInfoForUserDto, @Req() req: Request) {
+  async banUserForBlog(
+    @Param('id') id: string,
+    @Body() dto: BanInfoForUserDto,
+    @Req() req: Request,
+  ) {
     // const updateBlog = await this.commandBus.execute(new UpdateBlogCommand(id, dto, req.headers.authorization as string));
     // return updateBlog;
-    await this.blogsService.banUserForBlog(req.headers.authorization as string, dto)
+    await this.blogsService.banUserForBlog(
+      req.headers.authorization as string,
+      dto,
+    );
   }
 
+  @Get('users/blog/:id')
+  @HttpCode(200)
+  @UseGuards(JwtAuthGuard)
+  async getBannedUsersForBlog(
+    @Param('id') id: string,
+    @Body() dto: BanInfoForUserDto,
+    @Req() req: Request,
+  ) {
+    // const updateBlog = await this.commandBus.execute(new UpdateBlogCommand(id, dto, req.headers.authorization as string));
+    // return updateBlog;
+    // await this.blogsService.banUserForBlog(req.headers.authorization as string, dto)
+    return console.log(id);
+  }
 }
 
 
